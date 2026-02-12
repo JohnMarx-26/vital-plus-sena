677df88ff280385f440c662485bef81504d72119
JohnMarx-26 <john.chavarriaga@utp.edu.co>
2026-02-11 21:42:28 -0500

    Cambios y adicion de carpetas de estructura

---
+++ frontend/vitalplus-react/src/app/App.jsx
@@ -0,0 +1,9 @@
+export default function App(){
+  return(
+    <div className="text bg-green-700 flex items-center justify-center ">
+      <h1 className="text-white text-4xl font-bold">
+          Vital-Plus
+      </h1>
+    </div>
+  )
+}
