8638658c44cfdb481019dc37a95ddae5d1073042
JohnMarx-26 <john.chavarriaga@utp.edu.co>
2026-02-12 21:13:32 -0500

    actualizacion del proyecto

--- frontend/vitalplus-react/src/shared/components/Input.jsx
+++ frontend/vitalplus-react/src/shared/components/Input.jsx
@@ -1,44 +1,66 @@
 // Creacion de componente input
 
 export default function Input({label, type = "text", ...props}){
-    return(
-        <div className="w-^[320px]">
-            {/* Label */}
-            {label &&(
-             <label 
-                className="
+    return (
+      <div className="w-^[320px]">
+        {/* Label */}
+        {label && (
+          <label
+            className="
                 block
                 text-[8px]
                 mb-1
                 text-gray-600
                 "
-             >
-               {label}
-             </label>
-            )}
+          >
+            {label}
+          </label>
+        )}
 
         {/* El contenedor del input */}
+        <div
+          className="
+          relative
+          h-12
+          flex
+          items-center
+         "
+        >
+          {/* Area interactiva invisible(48px) */}
 
-        {/* Area interactiva visible(48px) */}
-
-        {/* Input visual */}
-        <input 
-            type={type} 
+          <div
             className="
+            absolute
+            inset-0
+            "
+           onMouseDown = {(e) =>{
+            e.preventDefault();
+            e.currentTarget.nextSibling.focus();
+            }}
+           
+          />
+
+          {/* Input visual */}
+          <input
+            type={type}
+            className="
+            relative
             w-full
-            h-12
+            h-8
             rounded-b-md
             border
             border-gray-300
             px-4
             text-base
-
-            focus:right-2
+            
+            focus:outline-none
+            focus:ring-2
             focus:ring-blue-500
             focus:border-blue-500
             "
-            {...props}     
-        />
+            {...props}
+          />
         </div>
+      </div>
     );
 }
