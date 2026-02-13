6a9805e99436b44e46a2e05ff4430d232d0c540a
JohnMarx-26 <john.chavarriaga@utp.edu.co>
2026-02-12 20:30:00 -0500

    Se actualiza proyecto

--- frontend/vitalplus-react/src/app/App.jsx
+++ frontend/vitalplus-react/src/app/App.jsx
@@ -1,9 +1,16 @@
+import UserForm from "../features/users/components/UserForm"
+
 export default function App(){
   return(
     <div className="text bg-green-700 flex items-center justify-center ">
       <h1 className="text-white text-4xl font-bold">
           Vital-Plus
       </h1>
+
+      <UserForm>
+        
+      </UserForm>
+
     </div>
   )
 }
