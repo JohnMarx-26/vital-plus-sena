9ac0706c308acccbcf34da98da00f6a94977a670
JohnMarx-26 <john.chavarriaga@utp.edu.co>
2026-02-13 22:33:02 -0500

    Cambios antes de actualizar main

--- frontend/vitalplus-react/src/app/App.jsx
+++ frontend/vitalplus-react/src/app/App.jsx
@@ -3,7 +3,7 @@ import UserForm from "../features/users/components/UserForm"
 export default function App(){
   return(
     <div className="grid grid-cols-1 gap-6 justify-items-center">
-      <h1 className="bg-blue-500 text-black text-2xl font-bold">
+      <h1 className="bg-blue-500 text-white text-2xl font-bold">
           Vital-Plus
       </h1>
 
