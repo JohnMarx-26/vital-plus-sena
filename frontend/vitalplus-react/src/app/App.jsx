bf85383305df314e15dcf487c77faee5446dee46
JohnMarx-26 <john.chavarriaga@utp.edu.co>
2026-02-26 18:16:59 -0500

    Actualizacion eventos

--- frontend/vitalplus-react/src/app/App.jsx
+++ frontend/vitalplus-react/src/app/App.jsx
@@ -1,5 +1,6 @@
 import CreateRoleForm from "../features/users/components/CreateRolForm";
 // import LoginForm from "../features/users/components/LoginForm";
+// import EventosReact from "../features/users/components/EventosReact";
 
 // import UserForm from "../features/users/components/UserForm";
 // import DeleteCounter from "../shared/components/DeleteCounter";
@@ -11,7 +12,7 @@ export default function App() {
       <h1 className="bg-brand text-brand-soft text-2xl font-sans font-bold">
         Vital-Plus
       </h1>
-
+      {/* <EventosReact/> */}
       <CreateRoleForm />
       
       {/* Otros componentes */}
