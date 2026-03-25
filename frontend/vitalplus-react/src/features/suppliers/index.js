9885532e580fbf2a8e5757452e25441329a68439
JohnMarx-26 <john.chavarriaga@utp.edu.co>
2026-03-25 16:05:49 -0500

    Ajustes

--- frontend/vitalplus-react/src/features/suppliers/index.js
+++ frontend/vitalplus-react/src/features/suppliers/index.js
@@ -4,3 +4,5 @@ export { default as SupplierDetailPage } from "./pages/SupplierDetailPage";
 export { default as ListSupplierPage } from "./pages/ListSupplierPage";
 
 export { default as SupplierForm } from "./components/SupplierForm";
+export { default as SupplierDataTable } from "./components/SupplierDataTable";
+export { default as SupplierDetailForm } from "./components/SupplierDetailForm";
